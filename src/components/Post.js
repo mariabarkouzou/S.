3e72594d2564421604d1a2ac581ce4832fb437cc
Maria Barkouzou <mbarkouzou@gmail.com>
2022-01-22 01:34:33 +0200

    Post Section Changes

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,29 +7,28 @@ export default function Post() {
     <>
       <div className="bg-white overflow-hidden mt-10 mb-10">
         <div className="relative max-w-7xl mx-auto px-4 lg:px-8">
-          <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
           <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-20 lg:max-w-none">
             <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
-              <figure>
-                <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
+             
+        
                   <img
-                    className="object-cover object-center"
+                    className="object-fill h-full w-full"
                     src={image}
                     alt="a woman holding a camera"
                   />
-                </div>
-              </figure>
+              
+             
             </div>
             <div>
-              <h2 className="mt-2 lg:text-base text-gray-100 font-extra-bold text-sm">
+              <h2 className="lg:mt-2 mt-5 lg:text-base text-gray-100 font-extra-bold text-sm">
                 Since the 1500s
               </h2>
               <h3 className="mt-2 lg:text-sxl font-extra-bold text-black text-blg">
                 The standard
               </h3>
-              <div className="mt-8 lg:mt-0">
+            
                 <div className="text-base max-w-prose mx-auto lg:max-w-none">
-                  <p className="lg:text-middle mt-5 text-base text-black">
+                  <p className="lg:text-middle lg:mt-5 text-base text-black">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -39,7 +38,7 @@ export default function Post() {
                     occaecat cupidatat non proident, sunt in culpa qui officia
                     deserunt.
                   </p>
-                </div>
+                
               </div>
             </div>
           </div>
@@ -52,12 +51,12 @@ export default function Post() {
         <div className="w-full lg:w-4/6">
           <div className="flex flex-col lg:flex-row-reverse rounded overflow-hidden h-auto lg:h-full">
             <img
-              className="block h-full w-full lg:w-1/2 flex-none bg-cover"
+              className="block h-auto w-full lg:w-1/2 flex-none bg-cover"
               src={imageSec}
               alt="a pillow"
             />
-            <div className="bg-white lg:mt-20 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-              <div className="text-black font-bold text-xl mb-2 leading-tight">
+            <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+              <div className="text-black lg:mt-16 md:mt-0 sm:mt-0 font-bold text-xl lg:mb-2 leading-tight">
                 <h2 className="mt-2 lg:text-base text-gray-100 font-extra-bold text-sm">
                   Since the 1500s
                 </h2>
